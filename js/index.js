1263fd7d7354144c68a6d882da1c63a1a5093efc
Roko2 <rokokovac77@gmail.com>
2021-05-23 23:03:32 +0200

    Navbar onhover, gotove kategorije->ng-repeat i ng-if

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
+ 
 const $dropdown = $(".dropdown");
 const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
-const showClass = "show";
- 
+const showClass = "collapse show";
+
 $(window).on("load resize", function() {
   if (this.matchMedia("(min-width: 768px)").matches) {
-    $dropdown.hover(
+    $(".dropdown").hover(
       function() {
+        console.log("da");
         const $this = $(this);
         $this.addClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "true");
-        $this.find($dropdownMenu).addClass(showClass);
+        $this.find($(".dropdown-toggle")).attr("aria-expanded", "true");
+        $this.find($(".dropdown-menu")).addClass(showClass);
       },
       function() {
         const $this = $(this);
         $this.removeClass(showClass);
-        $this.find($dropdownToggle).attr("aria-expanded", "false");
-        $this.find($dropdownMenu).removeClass(showClass);
+        $this.find($(".dropdown-toggle")).attr("aria-expanded", "false");
+        $this.find($(".dropdown-menu")).removeClass(showClass);
       }
     );
   } else {
